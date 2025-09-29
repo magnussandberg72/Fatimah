89befbd3b116bd4dc213ba02f74729599f098d34
magnussandberg72 <magnus.lulea@proton.me>
2025-09-29 23:08:10 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 // next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // fångar fler buggar i dev
-  swcMinify: false,      // 🔴 stänger av SWC (nödvändigt i Termux)
-
-  // Tillåt externa bilder om vi skulle behöva senare
-  images: {
-    domains: ["localhost"], 
-  },
-
-  // Internationalization (om du vill bygga på flera språk)
-  i18n: {
-    locales: ["en", "sv"],
-    defaultLocale: "en",
-  },
+  reactStrictMode: true,
+  swcMinify: false, // 🔴 stänger av SWC helt
 }
 
 module.exports = nextConfig
