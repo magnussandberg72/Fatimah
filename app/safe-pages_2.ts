6a7593a311663b1ed496513021f9877ed1dad093
magnussandberg72 <magnus.lulea@proton.me>
2025-09-29 19:58:40 +0200

    Create safe-pages_2.ts

---
+++ app/safe-pages_2.ts
@@ -0,0 +1,8 @@
+// Path: app/safe-pages_2.ts
+// 🔒 SafePages – Fatimah Project
+// Denna fil skyddar alla Fatimah-relaterade sidor från radering
+
+export const safePagesFatimah: readonly string[] = [
+  "/fatimah",
+  "/fatimah/presentation",
+]
