c339be019d9ed2bda553d7336f226990a4a14512
magnussandberg72 <magnus.lulea@proton.me>
2025-09-30 13:57:03 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Fatimah</h1>
+      <p>This is the first version of the site.</p>
+    </main>
+  )
+}
