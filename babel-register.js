fecd988d1790e5afcab7326873a878d9d0ad5d39
magnussandberg72 <magnus.lulea@proton.me>
2025-10-03 12:36:29 +0200

    Remove Babel config to use Next.js default SWC compiler

--- babel-register.js
+++
@@ -1,8 +0,0 @@
-require("@babel/register")({
-  extensions: [".js", ".jsx", ".ts", ".tsx"],
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    ["@babel/preset-react", { runtime: "automatic" }]
-  ],
-  ignore: [/node_modules/],
-});
